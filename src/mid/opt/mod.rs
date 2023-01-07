0f1312ca7c4a6f58778f44419ccb84c6f0ddb938
Karel Peeters <karel.peeters.leuven@gmail.com>
2023-01-07 15:49:40 +0100

    Port phi_pushing to new IR.

--- src/mid/opt/mod.rs
+++ src/mid/opt/mod.rs
@@ -5,7 +5,7 @@ pub mod flow_simplify;
 pub mod inline;
 pub mod dce;
 pub mod param_combine;
-// pub mod phi_pushing;
+pub mod phi_pushing;
 pub mod mem_forwarding;
 pub mod block_threading;
 // pub mod instr_simplify;
