c6a07c9b1c35da9af084dea674d513f17d5affe4
Karel Peeters <karel.peeters.leuven@gmail.com>
2023-01-09 22:35:36 +0100

    Fix bug in phi_pushing where a block could be changed multiple times.

--- src/mid/opt/phi_pushing.rs
+++ src/mid/opt/phi_pushing.rs
@@ -1,6 +1,8 @@
-use itertools::Itertools;
-use crate::mid::analyse::usage::BlockUsage;
+use std::collections::HashSet;
 
+use itertools::Itertools;
+
+use crate::mid::analyse::usage::BlockUsage;
 use crate::mid::analyse::use_info::UseInfo;
 use crate::mid::ir::{Program, Scoped, Target, Terminator, Value};
 use crate::util::VecExt;
@@ -11,20 +13,26 @@ use crate::util::VecExt;
 pub fn phi_pushing(prog: &mut Program) -> bool {
     let mut changed_terminators = 0;
 
+    // blocks that are blacklisted, either because it's the entry block or because we've changed this block already
+    // TODO can we do more stuff in a single pass?
+    let mut blacklist = HashSet::new();
+
     let use_info = UseInfo::new(prog);
 
     for func in use_info.funcs() {
         let func_entry = prog.get_func(func).entry;
+        blacklist.clear();
+        blacklist.insert(func_entry);
 
         for &block in use_info.func_blocks(func) {
             let block_info = prog.get_block(block);
 
-            // ensure we can skip this block: no instructions, jump terminator, no entry block on either side
-            if block == func_entry || !block_info.instructions.is_empty() {
+            // ensure we can skip this block: no instructions, jump terminator, no blacklist block on either side
+            if blacklist.contains(&block) || !block_info.instructions.is_empty() {
                 continue;
             }
             let target = if let Terminator::Jump { target } = &block_info.terminator {
-                if target.block == block || target.block == func_entry {
+                if target.block == block || blacklist.contains(&target.block) {
                     continue;
                 }
                 target.clone()
@@ -54,6 +62,7 @@ pub fn phi_pushing(prog: &mut Program) -> bool {
 
                 let (pred_pos, kind) = unwrap_match!(usage, BlockUsage::Target { pos, kind } => (pos, kind));
                 let target = kind.get_target_mut(&mut prog.get_block_mut(pred_pos.block).terminator);
+                blacklist.insert(pred_pos.block);
 
                 let Target { block: pred_target_block, args: old_pred_args } = target;
 
@@ -75,6 +84,7 @@ pub fn phi_pushing(prog: &mut Program) -> bool {
 
             // add phis to new block
             prog.get_block_mut(target_block).params.append(&mut old_params);
+            blacklist.insert(target_block);
         }
     }
 
