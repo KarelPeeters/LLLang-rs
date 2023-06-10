6e14d350e08f53251f0c471c31614fc1bd7c6519
Karel Peeters <karel.peeters.leuven@gmail.com>
2023-06-11 00:38:44 +0200

    Improve cond propagation for boolean comparisons.

--- src/mid/opt/cond_prop.rs
+++ src/mid/opt/cond_prop.rs
@@ -43,70 +43,117 @@ struct Replacement {
     complex: Value,
     simple: Value,
 
-    cond: ReplacementCondition,
+    cond: Condition,
 }
 
 #[derive(Debug, Copy, Clone)]
-struct ReplacementCondition {
+struct Branch {
     block: Block,
     true_block: Block,
     false_block: Block,
-    branch: bool,
 }
 
-impl ReplacementCondition {
+#[derive(Debug, Copy, Clone)]
+struct Condition {
+    branch: Branch,
+    cond: bool,
+}
+
+impl Condition {
     fn applies_to_instr_in(&self, dom_info: &DomInfo, block: Block) -> bool {
-        match self.branch {
-            true => dom_info.is_dominator(self.true_block, block) && !dom_info.is_reachable(self.false_block, block),
-            false => dom_info.is_dominator(self.false_block, block) && !dom_info.is_reachable(self.true_block, block),
+        match self.cond {
+            true => dom_info.is_dominator(self.branch.true_block, block) && !dom_info.is_reachable(self.branch.false_block, block),
+            false => dom_info.is_dominator(self.branch.false_block, block) && !dom_info.is_reachable(self.branch.true_block, block),
         }
     }
 
-    fn applies_to_branch_in(&self, block: Block, branch: bool) -> bool {
-        block == self.block && branch == self.branch
+    fn applies_to_target_in_branch(&self, block: Block, cond: bool) -> bool {
+        block == self.branch.block && cond == self.cond
     }
 }
 
-// TODO expand to more conditional cases, eg if we know `a == b` then we also know `a > b`.
+#[derive(Default)]
+struct Replacements {
+    replacements: Vec<Replacement>,
+}
+
+impl Replacements {
+    fn maybe_push_eq(&mut self, prog: &Program, branch: Branch, cond: bool, left: Value, right: Value) {
+        if let Some(pair) = sort_pair(prog, left, right) {
+            self.replacements.push(Replacement {
+                complex: pair.complex,
+                simple: pair.simple,
+                cond: Condition {
+                    branch,
+                    cond,
+                },
+            })
+        }
+    }
+
+    fn maybe_push_comparison(&mut self, prog: &Program, branch: Branch, kind: ComparisonOp, left: Value, right: Value) {
+        // TODO is all this extra complexity not unnecessary once we have bool expression simplification?
+        if prog.type_of_value(left) == prog.ty_bool() {
+            if let Some(right) = right.as_const().map(|c| c.as_bool().unwrap()) {
+                // comparison to boolean constant, we can add a lot of info from this
+                match kind {
+                    ComparisonOp::Eq => {
+                        self.maybe_push_eq(prog, branch, true, left, prog.const_bool(right).into());
+                        self.maybe_push_eq(prog, branch, false, left, prog.const_bool(!right).into());
+                    }
+                    ComparisonOp::Neq => {
+                        self.maybe_push_eq(prog, branch, true, left, prog.const_bool(!right).into());
+                        self.maybe_push_eq(prog, branch, false, left, prog.const_bool(right).into());
+                    }
+                    _ => {}
+                }
+
+                // we don't need to add the less complete replacements later
+                return;
+            }
+        }
+
+        // push replacement resulting from equality comparison
+        match kind {
+            ComparisonOp::Eq => self.maybe_push_eq(prog, branch, true, left, right),
+            ComparisonOp::Neq => self.maybe_push_eq(prog, branch, false, left, right),
+            _ => {}
+        }
+    }
+}
+
+// TODO simplify cond, left and right following other replacements?
+//    we can't just do that here, we need to visit blocks in dom order for that
+// TODO think about other expressions
+//    * eq. `a && b => a, b`
+// TODO and a more general "knowledge" system (ideally integrated into SCCP)
+//    * make more complicated evals, eg. `a > b => a >= b`
 fn cond_prop_func(prog: &mut Program, _: Function, use_info: &UseInfo, dom_info: &DomInfo) -> usize {
-    // collect replacement values
-    let mut replacements = vec![];
+    // collect replacements
+    let mut replacements = Replacements::default();
 
     for &block in dom_info.blocks() {
         if let &Terminator::Branch { cond, ref true_target, ref false_target } = &prog.get_block(block).terminator {
+            let branch = Branch { block, true_block: true_target.block, false_block: false_target.block };
+
+            // push immediate condition knowledge
+            replacements.maybe_push_eq(prog, branch, true, cond, prog.const_bool(true).into());
+            replacements.maybe_push_eq(prog, branch, false, cond, prog.const_bool(false).into());
+
             if let Some(cond) = cond.as_expr() {
                 if let &ExpressionInfo::Comparison { kind, left, right } = prog.get_expr(cond) {
-                    let eq_for_target = match kind {
-                        ComparisonOp::Eq => Some(true),
-                        ComparisonOp::Neq => Some(false),
-                        _ => None,
-                    };
-
-                    // TODO already simplify cond, left and right
-
-                    if let Some(eq_for_target) = eq_for_target {
-                        if let Some(pair) = sort_pair(prog, left, right) {
-                            replacements.push(Replacement {
-                                complex: pair.complex,
-                                simple: pair.simple,
-                                cond: ReplacementCondition {
-                                    block,
-                                    true_block: true_target.block,
-                                    false_block: false_target.block,
-                                    branch: eq_for_target,
-                                },
-                            })
-                        }
-                    }
+                    // push comparison knowledge
+                    replacements.maybe_push_comparison(prog, branch, kind, left, right);
                 }
             }
         }
     }
 
     // todo for a certain block, find the set of simplest replacements that apply
-    if !replacements.is_empty() {
+    // TODO remove these prints
+    if !replacements.replacements.is_empty() {
         println!("Replacements:");
-        for r in &replacements {
+        for r in &replacements.replacements {
             println!("  {:?}", r);
         }
     }
@@ -114,7 +161,7 @@ fn cond_prop_func(prog: &mut Program, _: Function, use_info: &UseInfo, dom_info:
     // apply replacements
     let mut replaced = 0;
 
-    for replacement in replacements {
+    for replacement in replacements.replacements {
         // TODO only apply replacement if it's the simplest one that applies
 
         replaced += use_info.replace_value_usages_if(prog, replacement.complex, replacement.simple, |_, usage| {
@@ -135,9 +182,9 @@ fn cond_prop_func(prog: &mut Program, _: Function, use_info: &UseInfo, dom_info:
                                 TargetKind::Jump =>
                                     replacement.cond.applies_to_instr_in(dom_info, pos.block),
                                 TargetKind::BranchTrue =>
-                                    replacement.cond.applies_to_branch_in(pos.block, true),
+                                    replacement.cond.applies_to_target_in_branch(pos.block, true),
                                 TargetKind::BranchFalse =>
-                                    replacement.cond.applies_to_branch_in(pos.block, false),
+                                    replacement.cond.applies_to_target_in_branch(pos.block, false),
                             }
                         }
                     }
